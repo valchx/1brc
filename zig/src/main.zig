6bf031d87fcb39f842518f9c3bc3035ead4a06dc
Valentin Charoux <valentin@charoux.com>
2025-12-16 22:06:26 +0400

    zig: blocking

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,27 +1,117 @@
 const std = @import("std");
-const zig = @import("zig");
+
+const Record = struct {
+    min: f32 = std.math.floatMax(f32),
+    max: f32 = std.math.floatMax(f32),
+    mean: f32 = 0,
+    count: u32 = 0,
+};
+
+fn readFileChunk(
+    file: std.fs.File,
+    seek: u64,
+    map_alloc: std.mem.Allocator,
+) !std.StringHashMap(Record) {
+    var map = std.StringHashMap(Record).init(map_alloc);
+
+    // TODO : Optimal buffer size ?
+    var buffer: [1028]u8 = undefined;
+    try file.seekTo(seek);
+    var reader_wrapper = file.reader(&buffer);
+    const reader: *std.Io.Reader = &reader_wrapper.interface;
+
+    try reader.readSliceAll(&buffer);
+
+    var index: u32 = 0;
+    while (reader.takeDelimiterExclusive('\n')) |line| {
+        reader.toss(1);
+
+        var line_iter = std.mem.splitAny(u8, line, ";");
+        const city_name = line_iter.next() orelse return error.ParseError;
+        const city_temp = brk: {
+            const str = line_iter.next() orelse return error.ParseError;
+
+            break :brk try std.fmt.parseFloat(f32, str);
+        };
+
+        const key = try map_alloc.alloc(u8, city_name.len);
+        @memmove(key, city_name);
+        const entry = try map.getOrPut((key));
+        if (entry.found_existing) {
+            entry.value_ptr.*.max = @max(entry.value_ptr.max, city_temp);
+            entry.value_ptr.*.min = @min(entry.value_ptr.min, city_temp);
+            entry.value_ptr.*.count += 1;
+            entry.value_ptr.*.mean += (city_temp - entry.value_ptr.*.mean) / @as(f32, @floatFromInt(entry.value_ptr.*.count));
+        } else {
+            entry.value_ptr.*.max = city_temp;
+            entry.value_ptr.*.min = city_temp;
+            entry.value_ptr.*.count = 1;
+            entry.value_ptr.*.mean = city_temp;
+        }
+
+        index += 1;
+    } else |err| {
+        switch (err) {
+            error.EndOfStream => {},
+            else => return error.ReaderError,
+        }
+    }
+
+    return map;
+}
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try zig.bufferedPrint();
-}
+    // - Split file in chunks
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const alloc = gpa.allocator();
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+    var args = try std.process.argsWithAllocator(alloc);
+    defer args.deinit();
 
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
+    // Ignore program name
+    _ = args.next();
+
+    const file_path = args.next() orelse @panic("File path not provided.");
+
+    const file = std.fs.cwd().openFile(
+        file_path,
+        .{ .mode = .read_only },
+    ) catch |err| {
+        std.debug.print("ERR: {any}", .{err});
+        @panic("Could not open file.");
     };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    defer file.close();
+
+    // const file_stat = file.stat() catch {
+    //     @panic("Could not stat file.");
+    // };
+
+    // file_stat.size // SPLIT FILE & CHUNKS
+
+    // - Read chunks in parallel
+    // - Compute min, mean and max temps per chunk
+    var map_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer map_arena.deinit();
+    var map = try readFileChunk(file, 0, map_arena.allocator());
+    defer map.deinit();
+
+    var entries = map.iterator();
+    while (entries.next()) |entry| {
+        const city_name = entry.key_ptr.*;
+        const city_data = entry.value_ptr.*;
+        std.debug.print(
+            "{s} : ({}, {}, {}, {})\n",
+            .{
+                city_name,
+                city_data.min,
+                city_data.mean,
+                city_data.max,
+                city_data.count,
+            },
+        );
+    } else {}
+
+    // - Aggregate all the chunks.
+
+    // - Print JSON
 }
